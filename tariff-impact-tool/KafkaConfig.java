3ebe787073d57531a42cf654a71e708c2aa97c02
Randy R <79384659+Merlins-Conquest@users.noreply.github.com>
2025-08-09 17:05:14 -0400

    Create KafkaConfig.java

---
+++ tariff-impact-tool/KafkaConfig.java
@@ -0,0 +1,8 @@
+package com.example.tariff_impact_tool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig { }
